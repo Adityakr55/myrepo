8715afe1a8929a905702a1d124c464e891001bef
AdityaKr55 <adityakumar58334@gmail.com>
2023-11-23 09:07:28 +0530

    New commit

---
+++ Hello.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void quicksort()
+{
+    //
+}
+void bubblesort()
+{
+    //
+}
+void heapsort()
+{
+    //
+}
+void insertionsort()
+{
+    //
+}
+int main()
+{
+    cout<<"Hello Aditya"<<endl;
+    return 0;
+}
